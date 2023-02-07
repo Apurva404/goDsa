dc86c3a6f92005dd5f52b90c25b00063236fe1ce
Apurva404 <apurvajha90@gmail.com>
2023-02-06 16:34:28 -0800

    add max heapify

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func getParent(i int) int {
+	return (i - 1) / 2
+}
+
+func getLeft(i int) int {
+	return 2*i + 1
+}
+
+func getRight(i int) int {
+	return 2 * (i + 1)
+}
+
+func swap(a int, b int) (int, int) {
+	return b, a
+}
+
+func maxHeapify(a []int, i int) {
+	heapSize := len(a)
+	l := getLeft(i)
+	r := getRight(i)
+	var largest int
+	if l < heapSize && a[l] > a[i] {
+		largest = l
+	} else {
+		largest = i
+	}
+	if r < heapSize && a[r] > a[largest] {
+		largest = r
+	}
+	fmt.Println(a[largest])
+	if largest != i {
+		a[i], a[largest] = swap(a[i], a[largest])
+		maxHeapify(a, largest)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Heap and different operations")
+	a := []int{1, 16, 14, 10, 7, 8, 9, 4, 2, 3}
+	fmt.Println(a)
+	maxHeapify(a, 0)
+	fmt.Println("after max heapify", a)
+
 }
